126d6c9c6ca03d9699933612f5d7de92e301175e
Alamito <alamirbobroski12@gmail.com>
2024-12-30 17:42:10 -0300

    remove useless code

--- client/mainClient.cpp
+++ client/mainClient.cpp
@@ -29,21 +29,3 @@ int main(int argc, char* argv[]){
         //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
 }
-
-    // SocketClient client(8080, "127.0.0.1");
-    // client.create();
-
-    // packet data, received;
-    // data.type = REQ;
-    // client.send(&data, sizeof(data));
-
-
-    // char buf[SIZE_BUFFER];
-    // struct sockaddr_in serverAddr;
-    // client.receive(buf, SIZE_BUFFER, &serverAddr);
-    // memcpy(&received, buf, sizeof(packet));
-
-    // cout << "Tipo do pacote recebido: " << received.type << endl;
-
-
-    // return 0;
